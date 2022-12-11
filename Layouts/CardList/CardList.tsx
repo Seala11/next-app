90fac4ae4c80033559ae50e379ac3842f562b006
Hanna Papova <seal.annuska@gmail.com>
2022-12-11 14:19:05 -0300

    fix: Minified React error

--- Layouts/CardList/CardList.tsx
+++ Layouts/CardList/CardList.tsx
@@ -1,18 +1,47 @@
-import React from 'react';
-import { IMovie } from '../../shared/api/types';
+import React, { useEffect } from 'react';
+import { fetchMovies } from '../../shared/api/movieApi';
+import { IMovie, IMovies } from '../../shared/api/types';
+import { useAppContext } from '../../shared/context/appProvider';
+import { LoadButton } from '../../shared/styles/sharedstyles';
 import MovieItem from './Card';
 import { FlexContainer } from './CardList.styled';
 
 type Props = {
-  movies: IMovie[];
+  movies: IMovies;
 };
 
 export default function CardList({ movies }: Props) {
+  const { moviesRes, setMoviesRes, page, setPage } = useAppContext();
+
+  useEffect(() => {
+    if (!moviesRes) {
+      setMoviesRes(movies.results);
+      setPage(movies.page);
+    }
+  }, [movies.page, movies.results, moviesRes, setMoviesRes, setPage]);
+
+  const dataHandler = async () => {
+    try {
+      const res = await fetchMovies(`${page + 1}`);
+      const movies: IMovies = await res.json();
+
+      const result = moviesRes.concat(
+        movies.results.filter((x) => !moviesRes.some((y) => y.id == x.id))
+      );
+      setMoviesRes(() => result);
+      setPage(page + 1);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
-    <FlexContainer>
-      {movies.map((movie) => (
-        <MovieItem key={movie.id} movie={movie} />
-      ))}
-    </FlexContainer>
+    <>
+      <FlexContainer>
+        {!moviesRes && <p>Loading</p>}
+        {moviesRes && moviesRes.map((movie) => <MovieItem key={movie.id} movie={movie} />)}
+      </FlexContainer>
+      <LoadButton onClick={dataHandler}>Load more</LoadButton>
+    </>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,39 +3,19 @@ import { InferGetStaticPropsType } from 'next';
 import CardList from '../Layouts/CardList/CardList';
 import { fetchMovies } from '../shared/api/movieApi';
 import { IMovies } from '../shared/api/types';
-import { useAppContext } from '../shared/context/appProvider';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function Movies({ movies }: InferGetStaticPropsType<typeof getStaticProps>) {
-  const { moviesRes, setMoviesRes, page, setPage } = useAppContext();
+  const [hydrated, setHydrated] = useState(false);
 
   useEffect(() => {
-    if (!moviesRes) {
-      setMoviesRes(movies.results);
-      setPage(movies.page);
-    }
-  }, [movies.page, movies.results, moviesRes, setMoviesRes, setPage]);
-
-  const dataHandler = async () => {
-    try {
-      const res = await fetchMovies(`${page + 1}`);
-      const movies: IMovies = await res.json();
-
-      const result = moviesRes.concat(
-        movies.results.filter((x) => !moviesRes.some((y) => y.id == x.id))
-      );
-      setMoviesRes(() => result);
-      setPage(page + 1);
-    } catch (err) {
-      console.error(err);
-    }
-  };
+    setHydrated(true);
+  }, []);
 
   return (
     <>
       <Title>Popular Movies</Title>
-      {moviesRes ? <CardList movies={moviesRes} /> : <CardList movies={movies.results} />}
-      <LoadButton onClick={dataHandler}>Load more</LoadButton>
+      {hydrated && <CardList movies={movies} />}
     </>
   );
 }
