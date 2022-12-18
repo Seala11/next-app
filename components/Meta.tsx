b6f22fb6fe3febd28950cb50997d20a1c0dd0f75
Hanna Papova <seal.annuska@gmail.com>
2022-12-18 16:19:01 -0300

    feat: add manifest.json, icons and update meta component

--- components/Meta.tsx
+++ components/Meta.tsx
@@ -10,11 +10,14 @@ type Props = {
 const Meta = ({ title, keywords, description }: Props) => {
   return (
     <Head>
+      <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name="keywords" content={keywords} />
       <meta name="description" content={description} />
-      <meta charSet="utf-8" />
+      <meta name="theme-color" content="#161d2f" />
       <link rel="icon" href="/favicon.ico" />
+      <link rel="manifest" href="/manifest.json" />
+      <link rel="apple-touch-icon" href="/icon.png"></link>
       <title>{title}</title>
     </Head>
   );
